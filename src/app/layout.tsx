e5f53bf8cb0185fe6cac3bcb14303e4afd674af4
Jose Carreiro <jmcgisc@gmail.com>
2025-08-12 23:19:31 -0600

    fix: google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -98,20 +98,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
   return (
     <html lang="es" className="scroll-smooth">
-      <body className={inter.className}>{children}
-      <script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"
-      >
-      </script>
-      
-      <script>
-        {`window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-XXXXXXXXXX');`}           
-      </script>
+      <head>
+      </head>
 
+      <body className={inter.className}>{children}
       <script
         dangerouslySetInnerHTML={{
           __html: `
@@ -119,14 +109,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-XXXXXX');
+            })(window,document,'script','dataLayer','GTM-TLSP8889');
           `,
         }}
       ></script>  
 
       <noscript>
         <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXX"
+          src="https://www.googletagmanager.com/ns.html?id=GTM-TLSP8889"
           height="0"
           width="0"
           style={{ display: 'none', visibility: 'hidden' }}
